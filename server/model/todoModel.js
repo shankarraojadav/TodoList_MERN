91fe5b382f823e176ec9fce35365c4606d8f8d30
SHANKAR JADAV <shankarjadav360@gmail.com>
2023-11-20 09:48:06 +0530

    changes

--- server/model/todoModel.js
+++ server/model/todoModel.js
@@ -1,5 +1,12 @@
 import mongoose from "mongoose";
 
+const getIndianTime = () => {
+  const now = new Date();
+  const utcOffset = 5.5 * 60; // India Standard Time (IST) UTC+5:30
+  const indianTime = new Date(now.getTime() + utcOffset * 60000);
+  return indianTime.toISOString().replace(/T/, ' ').replace(/\..+/, '');
+};
+
 const todoSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
@@ -7,11 +14,10 @@ const todoSchema = new mongoose.Schema({
     required: true,
   },
   title: { type: String, required: true },
-  createdAt: { type: String, default: new Date().toLocaleString() },
+  createdAt: { type: String, default: getIndianTime() },
   completedAt: { type: String, default: null },
   completed: { type: Boolean, default: false },
 });
-
 
 const Todo = mongoose.model("Todo", todoSchema);
 
