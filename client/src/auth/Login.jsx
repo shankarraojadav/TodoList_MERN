c434c0cd0553767fe9686643e1381c439d7157e2
SHANKAR JADAV <shankarjadav360@gmail.com>
2023-11-22 14:24:45 +0530

    better

--- client/src/auth/Login.jsx
+++ client/src/auth/Login.jsx
@@ -53,7 +53,7 @@ export default function Login() {
           <img
             width="30vh"
             src="https://res.cloudinary.com/dflhxdxgb/image/upload/v1700642658/google_x5kl6j.svg"
-            style={{ fontSize: "3vh", mr: "1vh" }}
+            style={{ fontSize: "3vh", mr: "1vh", cursor:"pointer" }}
           />
           Sign In With Google
         </p>
